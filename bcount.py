58b034fa637e63622b0d63b25f855ccd38c41fd5
saa938 <83087832+saa938@users.noreply.github.com>
2022-11-08 18:14:39 -0800

    Update bcount.py

--- bcount.py
+++ bcount.py
@@ -5,18 +5,17 @@ sys.stdin = open("bcount.in","r")
 sys.stdout = open("bcount.out","w")
 
 n,q = map(int, input().split())
-cows=[]
+
 s=[[0,0,0]]
 for i in range(n):
     x = int(input())
-    cows.append(x)
     if x == 1:
         s.append([s[-1][0]+1,s[-1][1],s[-1][2]])
     elif x == 2:
         s.append([s[-1][0],s[-1][1]+1,s[-1][2]])
     else:
         s.append([s[-1][0],s[-1][1],s[-1][2]+1])
-# print(s)
+
 for i in range(q):
     start,end = map(int, input().split())
     h,g,j=0,0,0
